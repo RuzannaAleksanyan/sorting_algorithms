b966542bc7dfcbd2462825127a00271ce467b1e0
RozAle <110930300+RuzannaAleksanyan@users.noreply.github.com>
2023-08-07 01:22:52 +0400

    Create selection_sort.hpp

---
+++ selection_sort.hpp
@@ -0,0 +1,23 @@
+#ifndef SELECTION
+#define SELECTION
+#include <vector>
+
+template <typename T>
+void selection_sort(std::vector<T>& vec) {
+    for(int i = 0; i < vec.size(); ++i) {
+        int min = i;
+        for(int j = i + 1; j < vec.size(); ++j) {
+            if(vec[j] < vec[min]) {
+                min = j;
+            }
+        }
+        
+        if(i != min) {
+            T tmp = vec[i];
+            vec[i] = vec[min];
+            vec[min] = tmp;
+        }
+    }
+}
+
+#endif
