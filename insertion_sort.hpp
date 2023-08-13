31b9f533f387dc55b49a7009db84dcf9f84862eb
RozAle <110930300+RuzannaAleksanyan@users.noreply.github.com>
2023-08-14 02:37:07 +0400

    Create insertion_sort.hpp

---
+++ insertion_sort.hpp
@@ -0,0 +1,20 @@
+#ifndef INSERTION
+#define INSERTION
+#include <vector>
+
+template <typename T>
+void insertion_sort(std::vector<T>& vec) {
+    int i, key, j;
+    for (i = 1; i < vec.size(); i++) {
+        key = vec[i];
+        j = i - 1;
+
+        while (j >= 0 && vec[j] > key) {
+            vec[j + 1] = vec[j];
+            j = j - 1;
+        }
+        vec[j + 1] = key;
+    }
+}
+
+#endif
