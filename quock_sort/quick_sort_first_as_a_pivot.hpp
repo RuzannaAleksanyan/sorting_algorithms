2e023e0faae4252c914d52e319c2521b30421ba5
RozAle <110930300+RuzannaAleksanyan@users.noreply.github.com>
2023-08-15 00:48:50 +0400

    Create quick_sort_first_as_a_pivot.hpp

---
+++ quock_sort/quick_sort_first_as_a_pivot.hpp
@@ -0,0 +1,32 @@
+#ifndef QUICK
+#define QUICK
+#include <vector>
+
+template <typename T>
+int partition(std::vector<T>& vec, int start, int end) {
+    if (vec.empty()) {
+        return;
+    }
+    
+    int pivot = vec[start];
+    int i = start - 1;
+    for(int j = start; j <= end - 1; ++j) {
+        if(vec[j] < pivot) {
+            ++i;
+            std::swap(vec[i], vec[j]);
+        }
+    }
+    std::swap(vec[i + 1], vec[end]);
+    return i;
+}
+
+template <typename T>
+void quick_sort(std::vector<T>& vec, int start, int end) {
+    if(start < end) {
+        int pi = partition(vec, start, end);
+        quick_sort(vec, start, pi - 1);
+        quick_sort(vec, pi + 1, end);
+    }
+}
+
+#endif
