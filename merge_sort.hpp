3837fe61c0c1b1efd859fd734eef8a06596014ac
Roz1204 <lifeiswanderful1234@gmail.com>
2025-03-03 15:04:46 +0400

    Merge sort implementation fix

--- merge_sort.hpp
+++ merge_sort.hpp
@@ -3,7 +3,7 @@
 #include <vector>
 
 template <typename T>
-void merge_sort(std::vector<T>& vec, int start, int mid, int end) {
+void merge(std::vector<T>& vec, int start, int mid, int end) {
     if (vec.empty()) {
         return;
     }
@@ -13,28 +13,30 @@ void merge_sort(std::vector<T>& vec, int start, int mid, int end) {
     int first2 = mid + 1;
     int last2 = end;
 
-    int* tmp = new int[end - start];
+    int* tmp = new int[end - start + 1];
     int index = 0;
 
-    while((first1 <= last1) && (first2 <= last2)) {
-        if(vec[first1] <= vec[first2]) {
+    while ((first1 <= last1) && (first2 <= last2)) {
+        if (vec[first1] <= vec[first2]) {
             tmp[index++] = vec[first1++];
         } else {
             tmp[index++] = vec[first2++];
         }
     }
 
-    while(first1 <= last1) {
+    while (first1 <= last1) {
         tmp[index++] = vec[first1++];
     }
 
-    while(first2 <= last2) {
+    while (first2 <= last2) {
         tmp[index++] = vec[first2++];
     }
 
-    for(int i = 0; i < end + 1; ++i) {
-        vec[i] = tmp[i];
+    for (int i = 0; i < (end - start + 1); ++i) {
+        vec[start + i] = tmp[i];
     }
+
+    delete[] tmp; 
 }
 
 #endif
